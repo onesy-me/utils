193af9615737402cc39169316bc24ffa2cdb5a96
Lazar Eric <lazareric.com@gmail.com>
2025-10-07 11:50:39 +0200

    m

--- src/capitalize.ts
+++ src/capitalize.ts
@@ -2,7 +2,7 @@ import is from './is';
 
 const capitalize = (value: string, words = false): string => {
   if (is('string', value)) {
-    if (words) value.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+    if (words) return value.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
 
     return `${value.charAt(0).toUpperCase()}${value.slice(1)}`;
   }
